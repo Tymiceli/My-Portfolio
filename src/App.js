adcad9e93cb87b4e08ebde9b54dafd8677e8ab9c
Tyler Miceli <tymiceli24@gmail.com>
2023-11-24 17:29:59 -0500

    fixed netlify redirect bug

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes, HashRouter } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './components/Home'
 import About from './components/About';
